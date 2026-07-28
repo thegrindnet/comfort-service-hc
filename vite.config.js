57cce69cd8bf430e2c9a48d91798d299f5803be1
David Bonilla <thegrindnet@gmail.com>
2026-07-27 19:54:52 -0600

    fix github pages base path

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/comfort-service-hc",
+  base: "/comfort-service-hc/",
   server: {
     port: 5173,
   },
